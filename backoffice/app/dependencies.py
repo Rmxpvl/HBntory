8eae8f8283a5578e786db3518ae58844727766bb
Rmxpvl <remy.pinville@gmail.com>
2026-07-29 15:18:33 +0200

    Revert "Add auth/passwords.py, update seed.py to use it (Task 2.2, done early)"

--- backoffice/app/dependencies.py
+++
@@ -1,9 +0,0 @@
-from .db import SessionLocal
-
-
-def get_db():
-    db = SessionLocal()  # Open a database session
-    try:
-        yield db         # Give it to the backend function (for example: add stock)
-    finally:
-        db.close()       # Close the session when the work is finished
